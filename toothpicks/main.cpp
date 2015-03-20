1c0042b2a90100b0cbb296b527bbac00223c3acf
Daisy Zhenyu Liu <bbc.abc@live.com>
2015-03-20 23:08:06 +1300

    Initial Commit

---
+++ toothpicks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  toothpicks
+//
+//  Created by daisy_island on 20/03/15.
+//  Copyright (c) 2015 daisy_island. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
